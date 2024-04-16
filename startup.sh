6d4de5c11b35811828d7c3f9705627602756082f
MikeEzane <mezaneus@gmail.com>
2024-04-16 00:30:33 -0400

    readme update

--- startup.sh
+++ startup.sh
@@ -7,3 +7,14 @@ git clone https://git.toptal.com/screening-ops/Mike-ezane.git
 cd /Mike-ezane
 pip install requirements.txt
 echo "*/10 * * * * $SHELL remediation.sh" | crontab -
+
+
+
+
+ ## As you can see on my VS code, my startup script as soon as I provision terraform,
+ ## and run terraform in it, terraform validate, terraform plan and terraform apply,
+ ## it will provision the resources on these configuration files on the left;
+ ## that is the compute resource, the IAM, the MFA, and networking resources. 
+ ## After he does this, the Cron job on line 9 of my startup script, as you can see here,
+ ## runs every 10 minutes and continuously monitors the environment. 
+ ## If any anomalies are detected, it would auto-remediate.
